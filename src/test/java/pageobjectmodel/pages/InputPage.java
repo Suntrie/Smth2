36ea86cb062dfa0aa0cca6ff46baef0d15bc0f07
Suntrie <inga.egorova@waveaccess.ru>
2023-04-30 02:29:54 +0200

    Location correction + new APK

--- src/test/java/pageobjectmodel/pages/InputPage.java
+++ src/test/java/pageobjectmodel/pages/InputPage.java
@@ -15,11 +15,15 @@ public class InputPage {
     }
 
     //https://www.browserstack.com/guide/locators-in-appium
-    //Xpath in Appium analyzes the XML structure of the app and then locates the element. Xpath should only be used when there is no ID, Name, or accessibility ID assigned to a specific UI element. Although XPath allows for the formulation of complex queries, using XPath is not recommended because it has stability and performance issues (as mentioned in the official documentation).
+    //Xpath in Appium analyzes the XML structure of the app and then locates the element.
+    // Xpath should only be used when there is no ID, Name, or accessibility ID assigned
+    // to a specific UI element. Although XPath allows for the formulation of complex queries,
+    // using XPath is not recommended because it has stability and performance issues
+    // (as mentioned in the official documentation).
     @AndroidFindBy(xpath = "//*[contains(@text,'Bar')]")
     private WebElement textView;
 
-    @AndroidFindBy(className = "androidx.compose.material3.TextField")
+    @AndroidFindBy(className = "android.widget.EditText")
     private WebElement textField;
 
     public void inputText(String input){
